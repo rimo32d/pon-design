231d6c30a4d6557bffc9fe35bff9a475afad8f3b
rimo32d <nv0f32d@gmail.com>
2022-06-14 17:45:10 +0900

    fv設定

---
+++ src/components/TheButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button
+      @click="$emit('onClick')"
+      type="button"
+      class="c-button"
+      :class="modifierClass"
+    >
+      {{ text }}
+    </button>
+</template>
+
+<script>
+export default({
+  props: {
+    text: { 
+      type: String,
+      default: "MORE",
+      required: false 
+    },
+    modifierClass: {
+      type: String,
+      default: "",
+      require: false
+    }
+  },
+  setup(context) {
+    const execEmit = () => {
+      context.emit('onClick');
+    }
+    return {
+      execEmit
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+  .c-button {
+    border-radius: 50px;
+  }
+  .c-button--white {
+    background-color: transparent;
+    border: 1px solid #fff;
+    padding: 20px 50px;
+    font-weight: 700;
+  }
+  .c-button--purple {
+    background-color: purple;
+  }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,43 @@
 <template>
   <div class="home">
+    <!-- ヘッダー -->
+    <section class="l-fv">
+      <div class="l-fv__title">
+        <h1 class="l-fv__title--large">
+          WEB DESIGN SPECIALIST
+        </h1>
+        <h2 class="l-fv__title--small">
+          お客様の夢を叶えるWebサイトを制作
+        </h2>
+        <TheButton
+          text="CONTACT"
+          class="l-fv__title"
+          modifier-class="c-button--white"
+        ></TheButton>
+      </div>
+      <div class="l-fv__scroll-arrow">
+        <div class="l-fv__scroll-arrow-text">
+          SCROLL
+        </div>
+        <span class="l-fv__scroll-arrow-mark"></span>
+      </div>
+    </section>
+    <section class="news"></section>
+    <section class="service"></section>
+    <section class="works"></section>
+    <section class="company"></section>
+    <section class="recruit"></section>
+    <section class="contact"></section>
+    <!-- フッター -->
   </div>
 </template>
 
 <script>
+import TheButton from '@/components/TheButton.vue';
 export default {
   name: 'HomePage',
+  components: {
+    TheButton
+  },
 }
 </script>
