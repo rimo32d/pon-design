f2fc7e78ff71d01d0c44f9fe14495c1c2286048f
rimo32d <nv0f32d@gmail.com>
2022-06-17 15:33:27 +0900

    修正

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,37 +3,37 @@
     <nav class="l-footer__nav">
       <ul class="l-footer__nav--list">
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/home" class="l-footer__nav--link">
             HOME
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/news" class="l-footer__nav--link">
             NEWS
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/service" class="l-footer__nav--link">
             SERVICE
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/works" class="l-footer__nav--link">
             WORKS
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/company" class="l-footer__nav--link">
             COMPANY
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/recruit" class="l-footer__nav--link">
             RECRUIT
           </a>
         </li>
         <li class="l-footer__nav--item">
-          <a href="#" class="l-footer__nav--link">
+          <a href="/contact" class="l-footer__nav--link">
             CONTACT
           </a>
         </li>
